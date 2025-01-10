4a3f2cd5d07c2e0a806cccb4aaec5dadd5e2cc18
Aldabarbosa96 <aldabarbosa1996@gmail.com>
2025-01-10 10:03:21 +0100

    v.1.0.1-alpha

--- core/src/main/java/com/sticklike/core/entities/Player.java
+++ core/src/main/java/com/sticklike/core/entities/Player.java
@@ -1,10 +1,10 @@
 package com.sticklike.core.entities;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.Input;
 import com.sticklike.core.screens.GameScreen;
 
 import java.util.ArrayList;
